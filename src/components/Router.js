2208e703d3e9dc4e766ac2c407d114af0f2e7e37
Evan Nichols <evantnichols@gmail.com>
2019-09-23 22:31:34 -0400

    props and state example placeholder

--- src/components/Router.js
+++ src/components/Router.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Link } from 'react-router-dom'
 import Fetch from './fetch/Fetch'
 import Home from './home/Home'
 import PasswordInput from './password/PasswordInput'
+import PropsAndState from './propsandstate/PropsAndState'
 
 const Router = () => (
   <BrowserRouter>
@@ -14,6 +15,9 @@ const Router = () => (
             <Link to="/">Home</Link>
           </li>
           <li>
+            <Link to="/props-and-state/">Props and State</Link>
+          </li>
+          <li>
             <Link to="/password/">Password Strength</Link>
           </li>
           <li>
@@ -22,6 +26,7 @@ const Router = () => (
         </ul>
       </nav>
       <Route path="/" exact={true} component={Home} />
+            <Route path="/props-and-state/"  component={PropsAndState} />
       <Route path="/password/" component={PasswordInput} />
       <Route path="/fetch-data/" component={Fetch} />
     </div>
