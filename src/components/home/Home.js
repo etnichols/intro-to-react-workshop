7c66dce0da272683f2bf2c4b627b4f9dc2c78fe9
Evan Nichols <evantnichols@gmail.com>
2020-03-29 11:39:04 -0500

    update Home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,8 +5,8 @@ const Home = () => {
   return (
     <div className="Container">
       <h2>Welcome to the workshop!</h2>
-      <p>The code for this demo is available at <a href="https://github.com/e-nichols/cuny-react-workshop">https://github.com/e-nichols/cuny-react-workshop</a>.</p>
-      <p>Follow the instructions in the <a href="https://github.com/e-nichols/cuny-react-workshop/blob/master/README.md">README</a> on how to clone and run this code on your own computer.</p>
+      <p>The code for this demo is available at <a href="https://github.com/e-nichols/intro-to-react-workshop">https://github.com/e-nichols/intro-to-react-workshop</a>.</p>
+      <p>Follow the instructions in the <a href="https://github.com/e-nichols/intro-to-react-workshop/blob/master/README.md">README</a> on how to clone and run this code on your own computer.</p>
     </div>
   )
 }
